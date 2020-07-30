5909f20015c13999091ff3a17534523745139314
monkata1 <monev2009@abv.bg>
2020-07-30 21:58:49 +0300

    Main Menu Scene

---
+++ Assets/Scripts/buttonManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour
+{
+
+    double rNumber;
+    public GameObject selectLvl;
+    public GameObject mainMenu;
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
+    public void PlayNow()
+    {
+        rNumber = Random.Range(0, 3);
+        Debug.Log(rNumber);
+        if (rNumber == 0) SceneManager.LoadScene("Beers");
+        else if ( rNumber == 1) SceneManager.LoadScene("Cocktails");
+        else if (rNumber == 2) SceneManager.LoadScene("Other Drinks");
+
+    }
+    public void LoadBeers()
+    {
+        SceneManager.LoadScene("Beers");
+    }
+    public void LoadCocktails()
+    {
+        SceneManager.LoadScene("Cocktails");
+    }
+    public void LoadOthers()
+    {
+        SceneManager.LoadScene("Other Drinks");
+    }
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        selectLvl.SetActive(false);
+    }
+    public void SelectLvl()
+    {
+        selectLvl.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
